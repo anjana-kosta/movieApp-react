70aa5812da543bf5bcdfc1b76ea003f3bc960889
Anjana <89970377+anjana-kosta@users.noreply.github.com>
2025-03-13 13:51:27 +0530

    all file upload
    new way one by one

---
+++ components/footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+
+    return (
+      <footer className="py-3 bg-dark footer">
+        <div className="container">
+          <p className="m-0 text-center text-white">Copyright &copy; Movie DB 2023 Created By Anjana</p>
+        </div>
+      </footer>
+    )
+  }
+  
+  
+  export default Footer
---
+++ pages/about.js
@@ -0,0 +1,173 @@
+import React from 'react'
+import { Row, Col } from 'reactstrap';
+
+// functional component - arrow function
+const About = () => {
+  const message = 'Hello World'
+  return (
+    <div title="Anjana - Learn More About Me">
+    <div className="about-page">
+    <Row className="mt-5">
+            <Col md="6">
+              <div className="left-side">
+                <h1 className="title fadein">Hello, Welcome</h1>
+                <h4 className="subtitle fadein">To About Page</h4>
+                <p className="subsubTitle fadein">Feel free to read short description about me.</p>
+              </div>
+            </Col>
+            <Col md="6">
+              <div className="fadein">
+                <p>My name is Anjana and I am an experienced software engineer and freelance developer. </p>
+                <p>
+                  I am from Jhansi Uttar pradesh
+                  I did my High School from Arya Kanya Pathshala Inter Colloege Kalpi in 2009
+                  I did my Intermediate From Dr. Ambedker Inter College Aata Jaluan in 2011
+                  I did my Diploma from Veerangna Jhalkari Bai Govt. Girls Polytechnic Jhansi & Branch Computer Science & Engineering In 2014
+                  I did my Btech from Kanpur Institute Of Technology & Branch Computer Science & Engineering In 2017
+                  My Final Year Project is Accident Alert System. it is android base project it is a group project
+                  my role in this project is as a designing.
+                  After completing my degree, I have Joined The first Company is shubhavi business solution as a SEO Executive in 2017
+                  and there My Roles And Responsibilties were All social Link Are created and maintain the all social user And generate the website report 
+                  and after that I joined second company the Sivo Overseas Technology as a Website Development in 2019 My roles And Responsbilities were website designing and development
+                  and then I joined company a2z Web Infotech as a wordpress Developver in 2020 and there My roles and responsibilties website designing and development and created 
+                  ecommerce website.
+                  My technical skill is javascript,Jqery,Angular, nodejs,nextjs, git,joomla,Bootstrap,CSS3,HTML5,Photoshop,Wordpress,SEO,Digital Marketing and Mysql.
+                                  </p>
+                                  <p>
+                                  My last project is LCPR Website Maintenance. Which I worked for almost 1 year. Three people were involved in this project. 
+                  This project has provided wireless services, communication services, information, and fixed offers.
+                  This project has 2 websites one is Legacy and the other is headless. 
+                  The legacy website has contained all forms and offers pages(dynamic pages), and the headless website contains all static pages(information pages). We worked only on the Legacy website.
+                  We handled both frontend and backend code. This website is based on angular and node technology.
+                  Frontend based on angular HTML CSS bootstrap typescript and backend based on node</p>
+
+                  <p>Frontend (Angular)= which had contain the two languages English and Spanish. what we did to the language translator i18n 
+                  attributes to provide Angular with the information needed for text translation. and used the SEO Service. 
+                  It had more responsive and more user-friendly and used the bootstrap library for the responsive screens.</p>
+
+                  <p>Backend (Node)= In which used separate components and used separate email templates for the form. </p>
+
+                  <p>BitBucket = This is used for import and export code. We had a bitbucket access account.  
+                  We used the "git pull" command to import the code and another way we can also the "git clone" command to import the code.
+                  we can send the code in bitbucket by command with rules and restrictions. used this command
+                  1. git add.
+                  2. git commit 
+                  3. git push and after creating the pull request and do merge the code in the development repository</p>
+
+                  <p>Back4app = Back4app is one of the most amazing open source backends as a Service provider which is helping developers
+                  to build extensible and scalable mobile and web applications at a rapid pace.
+                  You can retrieve the data via API requests, clone the database or download it in a JSON format.</p>
+
+                  <p>Freshdesk =Freshdesk is a cloud-based customer service software that helps businesses of all sizes to deliver stellar customer support.
+                  Track and manage incoming tickets from multiple channels into one single view.
+                  Freshdesk converts requests coming in via email</p>
+
+                  <p>Active Collab= ActiveCollab is the project management software that gives you complete control over your work - all tasks, communication, team members, and files. 
+                  All ticket is created by a client through this software. and we maintained the all tickets and responded to the ticket. We used to reply directly on the ticket… what is the status of the ticket</p>   
+                  
+                  <p>My role and responsibility =  I have handled both frontend and backend.
+                  1. solve the ticket like this-
+                  1.1 Text changes in both Spanish and English 
+                  1.2 Banner changes
+                  1.3 Offer changes based on the condition
+                  1.4 Change the JSON file as a database update recorded
+                  1.5 Create the page according to the requirement
+                  1.6 Create the form according to the requirement
+                  1.7 Used the API according to the requirement
+                  1.8 Add the subject related to the requirement API 
+                  1.9 Url redirection (legacy to headless)
+                  1.10 Create a new header and footer like headless for some pages
+                  1.11 Change the email triggering in the backend</p>
+
+                  <p>I have created more pages of this project which created the form in all the pages like-
+                  1.https://www2.libertypr.com/appletv-internal
+                  2.https://www2.libertypr.com/wowfi-internal/returns
+                  3.https://www2.libertypr.com/back-to-school
+                  4.https://www2.libertypr.com/3p-giftcard
+                  5.https://www2.libertypr.com/3p
+                  6.https://www2.libertypr.com/formulario480
+                  7.https://www2.libertypr.com/vi-acp-mobile</p>
+
+                  <p>This type of ticket is also like this -
+                  1. Not sending the email in Freshdesk (back4app license empty)
+                  2.Confirmation is not going to customer(add the email template in backend)
+                  3.New api endpoint is not working( add the subject of new form in exesting endpoint)
+                  4. Form submit button is not working 
+                  5. Attached is any file, pdf, or image  form button that is not working ( more than 2GB validation)
+                  6. ACP pdf error (all fields selected show  in the pdf )
+                  7. Error in Wowfi page  due to validation</p>
+
+
+
+                  <p>2. Deployment process:-I used to do deployment too. -:
+                  2.1 Create a build file by running this command "git build --prod".
+                  2.2 go to the folder select all files and did create the zip file.
+                  2.3 connect the FortiClient and then 
+                  2.4 connect the Winscp and add the IP address and then
+                  2.5 connect the putti and enter the username and password
+                  2.5 use all commands with restriction
+                  mkdir zip file name                      
+                  chmod 777 -r libertyprweb-new                
+                  cd libertyprweb-new                          
+                  rf -rm assets
+                  deploy the zip file 
+                  unzip libertyweb.zip
+                  clear
+                  cd ..come to pixnabi folder                
+                  sudo mv libertyprweb any backfile name     
+                  sudo mv libertyprweb-new libertyprweb  </p>
+                  
+
+                  <p>3. Status report: I used to send the daily status report of the manager and client</p>
+
+                  <p>4. Active Collab: I used to handle the Active collab. and moved the ticket according to the ticket status and inform to client</p>
+
+                
+              </div>
+            </Col>
+          </Row>
+    </div>
+  </div>
+
+  
+
+  )
+}
+// WHEN TO USE:
+// 1. for smaller components
+// 2. reusable components
+// 3. presentional components , patialy right, we can use HOOKS and specify state
+
+// const About = () => {
+//   const message = 'Hello World'
+//   return (
+//     <h1>Hello About Page - {message}</h1>
+//   )
+// }
+
+// const About = () => {
+//   const message = 'Hello World'
+//   return React.createElement('h1', null, 'I am genereting this with createElement')
+// }
+
+// unctional component - normal function
+// functional component - normal function
+// function About() {
+//   return (
+//     <h1>Hello About Page</h1>
+//   )
+// }
+
+
+// class About extends React.Component {
+//   render() {
+//     return (
+//       <h1>Hello I am class component</h1>
+//     )
+//   }
+// }
+
+
+
+
+export default About
